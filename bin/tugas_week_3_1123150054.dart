184ecfd5269306cfb080c13685b09fa0958446c4
Dandi Taufiqurrahman <dandi.taufiq50@gmail.com>
2025-10-03 21:47:52 +0700

    initial new project dart

---
+++ bin/tugas_week_3_1123150054.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_week_3_1123150054/tugas_week_3_1123150054.dart' as tugas_week_3_1123150054;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_week_3_1123150054.calculate()}!');
+}
---
+++ test/tugas_week_3_1123150054_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_week_3_1123150054/tugas_week_3_1123150054.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
